7aea4cb73caf70649f6c9506c6517568e41bc2b0
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-03 15:32:50 -0400

    Export solution diagnostics

--- packages/opus_analysis/__init__.py
+++ packages/opus_analysis/__init__.py
@@ -1,5 +1,11 @@
+from .diagnostics import analyze_solution
 from .graph import build_solution_graph
 from .patterns import detect_patterns
 from .timeline import build_program_timeline
 
-__all__ = ["build_solution_graph", "build_program_timeline", "detect_patterns"]
+__all__ = [
+    "analyze_solution",
+    "build_solution_graph",
+    "build_program_timeline",
+    "detect_patterns",
+]
