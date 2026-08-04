1ab1c5a695b53a16cf06d230ac6fe50a23aa6a63
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-04 09:34:08 -0400

    Export replay trace builder

--- packages/opus_analysis/__init__.py
+++ packages/opus_analysis/__init__.py
@@ -1,10 +1,12 @@
 from .diagnostics import analyze_solution
 from .graph import build_solution_graph
 from .patterns import detect_patterns
+from .replay import build_replay_trace
 from .timeline import build_program_timeline
 
 __all__ = [
     "analyze_solution",
+    "build_replay_trace",
     "build_solution_graph",
     "build_program_timeline",
     "detect_patterns",
