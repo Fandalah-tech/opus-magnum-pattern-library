2acdd2115143847d7a007fff35ef9b624a39398b
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-04 12:37:43 -0400

    Export engine simulator types

--- packages/opus_engine/__init__.py
+++ packages/opus_engine/__init__.py
@@ -1,14 +1,21 @@
+from .arm import ArmState, branch_offsets
 from .builder import build_initial_world
 from .model import Atom, Bond, Hex, Molecule, connected_components
+from .simulator import MotionProposal, SimulationError, Simulator
 from .world import World, WorldEvent
 
 __all__ = [
+    "ArmState",
     "Atom",
     "Bond",
     "Hex",
     "Molecule",
+    "MotionProposal",
+    "SimulationError",
+    "Simulator",
     "World",
     "WorldEvent",
+    "branch_offsets",
     "build_initial_world",
     "connected_components",
 ]
