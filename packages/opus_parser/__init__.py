7d922656eaaf4b1ffd44c285a91bade2da475271
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-05 13:00:51 -0400

    Export solution writer API

--- packages/opus_parser/__init__.py
+++ packages/opus_parser/__init__.py
@@ -1,11 +1,15 @@
 from .binary import ParseError
 from .puzzle import parse_puzzle, parse_puzzle_bytes
 from .solution import parse_solution, parse_solution_bytes
+from .solution_writer import SolutionWriteError, write_solution, write_solution_bytes
 
 __all__ = [
     "ParseError",
+    "SolutionWriteError",
     "parse_puzzle",
     "parse_puzzle_bytes",
     "parse_solution",
     "parse_solution_bytes",
+    "write_solution",
+    "write_solution_bytes",
 ]
