6f2361c99a29dc41da0145cad67c1f94b163941b
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-05 16:03:13 -0400

    Export disjoint product planner

--- packages/opus_solver/__init__.py
+++ packages/opus_solver/__init__.py
@@ -1,3 +1,4 @@
+from .disjoint_plan import DisjointProductPlan, ProductComponent, build_disjoint_product_plan
 from .explorer import ExplorationResult, enumerate_joint_actions, explore_simulator_states
 from .manufacturing import (
     AtomFlow,
@@ -16,12 +17,15 @@ from .state import canonical_state_key
 
 __all__ = [
     "AtomFlow",
+    "DisjointProductPlan",
     "ExplorationResult",
     "GeneratedSolutionError",
     "ManufacturingOperation",
     "ManufacturingPlan",
+    "ProductComponent",
     "SolveResult",
     "UnsupportedPuzzleError",
+    "build_disjoint_product_plan",
     "build_manufacturing_plan",
     "canonical_state_key",
     "enumerate_joint_actions",
