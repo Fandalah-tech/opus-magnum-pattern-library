fb61bb4fd2e873d63fadf27982ba158efd8fc8b4
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-09 08:45:12 -0400

    Export fragment chain planner

--- packages/opus_solver/__init__.py
+++ packages/opus_solver/__init__.py
@@ -1,3 +1,4 @@
+from .composition import build_composition_prior, rank_fragment_chains
 from .library import build_solver_index, pareto_frontier
 from .manufacturing import (
     AtomFlow,
@@ -21,11 +22,13 @@ __all__ = [
     "ManufacturingPlan",
     "SolveResult",
     "UnsupportedPuzzleError",
+    "build_composition_prior",
     "build_manufacturing_plan",
     "build_solver_index",
     "mechanism_compatibility",
     "pareto_frontier",
     "puzzle_similarity",
+    "rank_fragment_chains",
     "rank_mechanisms",
     "solve_puzzle",
     "validate_generated_solution",
