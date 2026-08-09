429e18b86ae2bd3c4c83fd189eb09f48fea45ede
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-09 08:28:18 -0400

    Export functional fragment analysis

--- packages/opus_analysis/__init__.py
+++ packages/opus_analysis/__init__.py
@@ -1,5 +1,6 @@
 from .canonical import canonical_solution_hash, canonical_solution_payload
 from .diagnostics import analyze_solution
+from .fragments import extract_solution_fragments, functional_role
 from .graph import build_solution_graph
 from .patterns import detect_patterns
 from .puzzle_features import canonical_molecule_hash, puzzle_feature_fingerprint, puzzle_feature_payload
@@ -15,6 +16,8 @@ __all__ = [
     "canonical_solution_hash",
     "canonical_solution_payload",
     "detect_patterns",
+    "extract_solution_fragments",
+    "functional_role",
     "puzzle_feature_fingerprint",
     "puzzle_feature_payload",
 ]
