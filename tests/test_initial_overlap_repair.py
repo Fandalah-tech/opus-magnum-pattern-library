2d63a16d7478105ee364084046746cbba9e7cc1b
Fandalah-tech <bruno.boudreau@gmail.com>
2026-08-19 10:57:08 -0400

    Test length-changing arm overlap repair

--- tests/test_initial_overlap_repair.py
+++ tests/test_initial_overlap_repair.py
@@ -30,9 +30,44 @@ class InitialOverlapRepairTests(unittest.TestCase):
         arm = updated["parts"][0]
         self.assertEqual(arm["position"], [9, 0])
         self.assertEqual(arm["rotation"], 1)
+        self.assertEqual(arm["length"], 1)
         self.assertEqual(solution["parts"][0]["position"], [8, 1])
         repair = updated["source"]["initialArmBaseRepairs"][0]
         self.assertEqual(repair["preservedTip"], [9, 1])
+        self.assertEqual(repair["oldLength"], 1)
+        self.assertEqual(repair["newLength"], 1)
+        self.assertEqual(repair["targetSolutionBytesUsed"], 0)
+
+    def test_can_lengthen_arm_while_preserving_tip(self) -> None:
+        solution = {
+            "source": {},
+            "parts": [{
+                "id": "cleanup",
+                "type": "arm1",
+                "position": [8, 1],
+                "rotation": 0,
+                "length": 1,
+                "armNumber": 2,
+                "program": [{"cycle": 10, "instruction": "grab"}],
+            }],
+        }
+
+        updated = relocate_arm_base_preserving_tip(
+            solution,
+            arm_part_id="cleanup",
+            preserved_tip=(9, 1),
+            new_rotation=0,
+            new_length=2,
+        )
+
+        arm = updated["parts"][0]
+        self.assertEqual(arm["position"], [7, 1])
+        self.assertEqual(arm["rotation"], 0)
+        self.assertEqual(arm["length"], 2)
+        repair = updated["source"]["initialArmBaseRepairs"][0]
+        self.assertEqual(repair["preservedTip"], [9, 1])
+        self.assertEqual(repair["oldLength"], 1)
+        self.assertEqual(repair["newLength"], 2)
         self.assertEqual(repair["targetSolutionBytesUsed"], 0)
 
 
